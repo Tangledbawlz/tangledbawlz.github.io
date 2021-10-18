94254d47643298ea28d20111521ad99fae8b1efb
N3rdman <ntangedal@gmail.com>
2021-10-17 18:21:41 -0600

    commit

---
+++ Other Code/main.js
@@ -0,0 +1,25 @@
+
+var name = window. prompt("Enter your name: "); alert("Your name is " + name);
+
+
+
+
+
+let item = null;
+let numbCrypt = null;
+
+
+
+
+function cesearEncryption (item, numbCrypt)
+{
+    let encryption = item; //casting user input to a string value
+    for(let i = 0; i < encryption.length; i++)
+    {
+       // = "item".charAt(i) + numbCrypt; //iterate through length of the users value and encrypt code
+
+    }
+
+    
+    
+}
